e5a41fd0420efe179c5412405d942dc77c366b60
Caine Spiwak <143353083+Caine-Spiwak@users.noreply.github.com>
2023-12-01 22:41:20 -0800

    modularize the javascript

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 // DE-MODULARIZE TO VIEW
 
-// const dropdowns = () => {
+const dropdowns = () => {
     const dropdownLists = document.querySelectorAll(".dropdown-list")
 
     dropdownLists.forEach((dropdown) => {
@@ -27,9 +27,9 @@
         })
 
     });
-// };
+};
 
-// export default dropdowns;
+export default dropdowns;
 
 // DE-MODULARIZE TO VIEW
 
